2f26e9f8b3b8d9c89ce20bf4d540319f522dbcb0
sanav33 <sanyaverma0303@gmail.com>
2022-07-19 17:19:57 -0400

    gj <3 old fart

--- api/match.py
+++ api/match.py
@@ -1,17 +1,19 @@
-import app
+from flask import Blueprint, jsonify
 import pymongo
 
+match_bp = Blueprint('match', __name__)
 
-@app.route('/match', methods=['POST'])
+
+@match_bp.route('/match', methods=['POST'])
 def match():
     client = pymongo.MongoClient(
         'mongodb+srv://admin:admin@matchatcluster.mixvt.mongodb.net/?retryWrites=true&w=majority')
     db = pymongo.database.Database(client, 'matchat')
     profiles = db['profiles']
     internsCursor = profiles.find({"opt_in": True, "is_intern": True}, projection=[
-                                  "prefers", "met_with", "timezone"])
+                                  "prefers", "met_with", "timezone", "name"])
     ftesCursor = profiles.find({"opt_in": True, "is_intern": False}, projection=[
-                               "prefers", "met_with", "timezone"])
+                               "prefers", "met_with", "timezone", "name"])
     interns = {}
     ftes = {}
     assignments = {}
@@ -20,6 +22,7 @@ def match():
             'prefers': intern['prefers'],
             'met_with': intern['met_with'],
             'timezone': intern['timezone'],
+            'name': intern['name'],
         }
 
     for fte in ftesCursor:
@@ -27,17 +30,20 @@ def match():
             'prefers': fte['prefers'],
             'met_with': fte['met_with'],
             'timezone': fte['timezone'],
+            'name': fte['name'],
         }
 
+    print('interns: ', interns)
+    print('ftes', ftes)
     # naming: matches_x_y -> x wants y
     # list of intern IDs who prefer FTEs
-    matches_intern_ftes = [id for id, v in interns if v['prefers']
+    matches_intern_ftes = [id for id, v in interns.items() if v['prefers']
                            ['ftes'] and not v['prefers']['interns']]
 
-    matches_fte_ftes = [id for id, v in ftes if v['prefers']
+    matches_fte_ftes = [id for id, v in ftes.items() if v['prefers']
                         ['ftes'] and not v['prefers']['interns']]
 
-    matches_intern_interns = [id for id, v in interns if v['prefers']
+    matches_intern_interns = [id for id, v in interns.items() if v['prefers']
                               ['interns'] and not v['prefers']['ftes']]
 
     no_match = []
@@ -48,56 +54,57 @@ def match():
     ftes_remaining = []
     interns_remaining = []
 
-    for fte in ftes:
+    for id, fte in ftes.items():
         # match ftes who are only interested in interns to interns
         if matches_intern_ftes and fte['prefers']['interns'] and not fte['prefers']['ftes']:
             # get intern that only wants fte
             assignee = matches_intern_ftes.pop()
             # assign fte that only wants intern -> intern that only wants fte
-            assignments[fte['_id']] = assignee
-            assignments[assignee] = fte['_id']
+            assignments[id] = assignee
+            assignments[assignee] = id
 
         # match ftes who are only interested in other ftes to interns
         elif matches_fte_ftes and fte['prefers']['ftes'] and not fte['prefers']['interns']:
             # find an fte that only wants ftes BUT is not the current fte
             for f in matches_fte_ftes:
-                if f == fte['_id'] or f in assignments or already_met_recently(ftes[f], fte):
+                if f == id or f in assignments or already_met_recently(ftes[f], fte):
                     continue
                 assignee = f
                 break
             matches_fte_ftes.remove(f)
             # assign fte that only wants fte -> fte that only wants fte
-            assignments[fte['_id']] = assignee
-            assignments[assignee] = fte['_id']
+            assignments[id] = assignee
+            assignments[assignee] = id
 
         else:
-            ftes_remaining.append(fte)
+            ftes_remaining.append(id)
 
-    for intern in interns:
+    for id, intern in interns.items():
+        print(id)
         # match interns who are only interested in other interns to interns
-        if matches_intern_interns and interns['prefers']['interns'] and not interns['prefers']['ftes']:
+        if matches_intern_interns and intern['prefers']['interns'] and not intern['prefers']['ftes']:
             # find an intern that only wants interns BUT is not the current intern
             for i in matches_intern_interns:
-                if i == i['_id'] or i in assignments or already_met_recently(interns[f], intern):
+                if i == id or i in assignments or already_met_recently(interns[f], intern):
                     continue
                 assignee = i
                 break
             matches_intern_interns.remove(i)
             # assign intern that only wants interns -> intern that only wants interns
-            assignments[intern['_id']] = assignee
-            assignments[assignee] = intern['_id']
+            assignments[id] = assignee
+            assignments[assignee] = id
         else:
-            interns_remaining.append(intern)
+            interns_remaining.append(id)
 
-    matches_intern_both = [id for id, v in interns if v['prefers']
+    matches_intern_both = [id for id, v in interns.items() if v['prefers']
                            ['ftes'] and v['prefers']['interns']]
-    matches_fte_both = + [id for id, v in ftes if v['prefers']
-                          ['ftes'] and not v['prefers']['interns']]
+    matches_fte_both = [id for id, v in ftes.items() if v['prefers']
+                        ['ftes'] and not v['prefers']['interns']]
 
     for fte in ftes_remaining:
-        if fte['prefers']['interns'] and not fte['prefers']['ftes']:
+        if ftes[fte]['prefers']['interns'] and not ftes[fte]['prefers']['ftes']:
             assignee = matches_intern_both.pop()
-        elif fte['prefers']['ftes'] and not fte['prefers']['interns']:
+        elif ftes[fte]['prefers']['ftes'] and not ftes[fte]['prefers']['interns']:
             assignee = matches_fte_both.pop()
         else:
             if matches_intern_both:
@@ -108,13 +115,13 @@ def match():
                 no_match.append(fte)
                 continue
 
-        assignments[fte['_id']] = assignee
-        assignments[assignee] = fte['_id']
+        assignments[fte] = assignee
+        assignments[assignee] = fte
 
     for intern in interns_remaining:
-        if intern['prefers']['interns'] and not intern['prefers']['ftes']:
+        if matches_intern_both and interns[intern]['prefers']['interns'] and not interns[intern]['prefers']['ftes']:
             assignee = matches_intern_both.pop()
-        elif intern['prefers']['ftes'] and not intern['prefers']['interns']:
+        elif matches_fte_both and interns[intern]['prefers']['ftes'] and not interns[intern]['prefers']['interns']:
             assignee = matches_fte_both.pop()
         else:
             if matches_intern_both:
@@ -125,13 +132,16 @@ def match():
                 no_match.append(intern)
                 continue
 
-        assignments[intern['_id']] = assignee
-        assignments[assignee] = intern['_id']
+        assignments[intern] = assignee
+        assignments[assignee] = intern
 
-# add assignments to met_with field in DB
+    print_assignments(assignments, ftes, interns)
+
+    return jsonify(success=True, status_code=200)
+
+# TODO: add assignments to met_with field in DB
+# TODO: check if ppl are on same team
 # send slack notifs
-    # for assignment in assignments:
-    #     if
 
 
 def already_met_recently(employee_1, employee_2) -> bool:
@@ -144,3 +154,10 @@ def already_met_recently(employee_1, employee_2) -> bool:
             return True
 
     return False
+
+
+def print_assignments(assignments, ftes, interns):
+    ftes.update(interns)
+    print(ftes)
+    for x, y in assignments.items():
+        print(ftes[x]['name'], ftes[y]['name'])
--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
 
+from api.match import match_bp
+
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=['GET'])
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+app.register_blueprint(match_bp)
