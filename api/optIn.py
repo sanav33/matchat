49af34d2f5636bf4f4655daf7f822a28d4f609f2
Jason Ngassa <jason.ngassa@mongodb.com>
2022-07-20 11:17:47 -0400

    Added text reminder component, still WIP

---
+++ api/optIn.py
@@ -0,0 +1,66 @@
+import logging
+import os
+# Import WebClient from Python SDK (github.com/slackapi/python-slack-sdk)
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+def sendOptInHelper(client, logger, user):
+    try:
+    # Call the chat.postMessage method using the WebClient
+        blocks = [
+            {
+                        "type": "section",
+                        "text": {
+                                "type": "mrkdwn",
+                                "text": "Hello @{name}!\n Would you like to Opt into next week's Coffee Roulette?".format(name = user['name'])
+                        }
+                },
+                {
+                        "type": "actions",
+                        "elements": [
+                                {
+                                        "type": "button",
+                                        "text": {
+                                                "type": "plain_text",
+                                                "text": "Yes!",
+                                                "emoji": True
+                                        },
+                                        "url": "https://google.com"
+                                },
+                                {
+                                        "type": "button",
+                                        "text": {
+                                                "type": "plain_text",
+                                                "text": "No...",
+                                                "emoji": True
+                                        }
+                                }
+                        ]
+                }
+        ]
+        result = client.chat_postMessage(
+            channel=user['id'],
+            text=":)",
+            blocks=blocks
+        )
+        logger.info(result)
+
+    except SlackApiError as e:
+        logger.error(f"Error posting message: {e}") 
+
+def sendOptIn(client, logger):
+    try:
+        result = client.users_list()
+        users_array = result["members"]
+        for user in users_array:
+            if (user["id"] == "U03Q4H2MNCT"):
+            # if (True):
+                sendOptInHelper(client, logger, user)
+    except SlackApiError as e:
+        logger.error("Error creating conversation: {}".format(e))
+
+if __name__ == '__main__':
+    client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
+    logger = logging.getLogger(__name__)
+    sendOptIn(client, logger)
+
