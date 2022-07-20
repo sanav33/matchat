7930a1d5cc8ff449648bf7cd3cc53e7568ad7667
GJ Isombe <guyjacques.isombe@mongodb.com>
2022-07-20 18:28:12 -0400

    finished home tab

--- api/home.py
+++ api/home.py
@@ -3,7 +3,7 @@ from os import environ
 import requests
 from models.profile import Profile
 from pymongo import MongoClient
-import block_kits.home
+from block_kits.home import *
 from utils.constants import ATLAS_CONNECTION_STR
 from json import dumps
 
@@ -15,38 +15,35 @@ home = Blueprint('home', __name__)
 @home.post('/home')
 def get_profile_handler():
     json_body = request.json
-    user_id = json_body["user"]
-    print("here")
+    slack_id = json_body["user"]
 
     # extract from profiles_coll
     profiles_coll = mongo_client.matchat.profiles
-    profile_doc = profiles_coll.find_one(user_id)
-    print("here")
+    profile_doc = profiles_coll.find_one({"slack_id": slack_id})
 
     if profile_doc is None:
         print("No profile created")
         return Response(
-            response=dumps(home.UNCREATED_PROFILE_HOME),
-            status_code=200,
+            response=dumps(UNCREATED_PROFILE_HOME),
+            status=200,
             content_type="application/json"
         )
     
     return Response(
-        response=dumps(home.created_profile_home(
+        response=dumps(created_profile_home(
             Profile(
+                slack_id=slack_id,
                 name = profile_doc["name"],
                 is_intern = profile_doc["is_intern"],
                 team_id = profile_doc["team_id"],
-                prefers = {
-                    "interns": profile_doc["prefers"]["interns"],
-                    "ftes": profile_doc["prefers"]["ftes"]
-                },
+                prefers_interns = profile_doc["prefers"]["interns"],
+                prefers_ftes = profile_doc["prefers"]["ftes"],
                 is_active = profile_doc["is_active"],
                 opt_in = profile_doc["opt_in"],
                 met_with = profile_doc["met_with"]
             )
         )),
-        status_code=200,
+        status=200,
         content_type="application/json"
     )
 
