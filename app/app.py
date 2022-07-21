481b90efdd110cc8f1b24d1600f195cb342541c4
GJ Isombe <guyjacques.isombe@mongodb.com>
2022-07-21 01:03:05 -0400

    adding challenge response

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 import profile
-from flask import Flask
+from flask import Flask, request, Response
 from app.api.profile import profile, profile_view
 from app.api.match import match_bp
 from app.api.home import home
@@ -7,9 +7,13 @@ from app.api.home import home
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET'])
+@app.get("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.post("/")
+def challenge_handler():
+    return request.json["challenge"]
 
 app.register_blueprint(profile_view)
 app.register_blueprint(profile)
